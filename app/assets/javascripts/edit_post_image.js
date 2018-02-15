8e780f2090125dd4080c96467bd6ff852f167001
maddale <maddale2@yandex.ru>
2018-02-16 03:25:58 +0700

    First backup in Russia

--- app/assets/javascripts/edit_post_image.js
+++ app/assets/javascripts/edit_post_image.js
@@ -1,9 +1,12 @@
+var image
+var originalWidth, originalHeight, newWidth, newHeight;
+
 function PhotoPickUp (event, url) {
   event.preventDefault();
-  var image = document.createElement('img');
+  image = document.createElement('img');
   image.src = url;
   image.id = 'cropbox';
-  var originalWidth, originalHeight, newWidth, newHeight;
+  
 
   $('#selected-photo').append(image);  
 
@@ -13,6 +16,7 @@ function PhotoPickUp (event, url) {
     originalHeight = image.naturalHeight;
     newWidth = image.width;
     newHeight = image.height;
+    console.log (originalWidth + ' ' + originalHeight + ' ' + newWidth + ' ' + newHeight);
   });
 
   $('#cropbox').Jcrop({
@@ -24,16 +28,38 @@ function PhotoPickUp (event, url) {
 
 
   function updateCoords(coords) {
-    $('#coord_x').val((coords.x / newWidth) * originalWidth);
-    $('#coord_y').val((coords.y / newHeight) * originalHeight);
-    $('#coord_w').val((coords.w / newWidth) * originalWidth);
-    $('#coord_h').val((coords.h / newHeight) * originalHeight);
-  }
-
-  function applyCrop() {
-    
-    $('#selected-photo').append(image);  
-    $('#popup-content-1').fadeOut(300);  
+    $('#coord_x').val(coords.x);
+    $('#coord_y').val(coords.y);
+    $('#coord_w').val(coords.w);
+    $('#coord_h').val(coords.h);
   }
 
 }
+
+  function applyCrop() {
+    image.id = 'picked-image'
+    var viewWidth = $('.post-title-edit').width();
+    var viewHeight = $('.post-title-edit').height();
+    var lastWidth = (viewWidth / (newWidth - $('#coord_w').val() + $('#coord_x').val())) * newWidth;
+    var aspectRatio = newHeight / newWidth;
+    var lastHeight = lastWidth * aspectRatio;
+
+    console.log ('viewWidth = ' + viewWidth);
+    console.log ('viewHeight = ' + viewHeight);
+    console.log ('newWidth = ' + newWidth);
+    console.log ('newHeight = ' + newHeight);
+    console.log ('coored x = ' + $('#coord_x').val());
+    console.log ('coord w = ' + $('#coord_w').val());
+    console.log ('last width = ' + lastWidth);
+
+
+    $('.post-title-edit').append(image);  
+    $('#popup-1').detach();  
+    $('#picked-image').css("width", lastWidth);
+    $('#picked-image').css("height", lastHeight);
+    $('#picked-image').css("top", ($('#coord_y').val() / newHeight) * lastHeight * -1);
+    $('#picked-image').css("left", ($('#coord_x').val() / newWidth) * lastWidth * -1);
+    $('#picked-image').css("display", "block");
+    $('#picked-image').css("visibility", "unset");
+
+  }
